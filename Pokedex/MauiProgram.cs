b0ba8ed532e16bb8bc0548eefdd2cc305c6a3c97
Eduardo Reis <eduardoreisdev@gmail.com>
2022-09-12 21:43:46 -0300

    WIP

--- Pokedex/MauiProgram.cs
+++ Pokedex/MauiProgram.cs
@@ -10,8 +10,25 @@ public static class MauiProgram
 
         builder.UseMauiApp<App>().ConfigureFonts(fonts =>
         {
+            fonts.AddFont("OpenSans-Bold.ttf", "OpenSansBold");
+            fonts.AddFont("OpenSans-BoldItalic.ttf", "OpenSansBoldItalic");
+
+            fonts.AddFont("OpenSans-ExtraBold.ttf", "OpenSansExtraBold");
+            fonts.AddFont("OpenSans-ExtraBoldItalic.ttf", "OpenSansExtraBoldItalic");
+
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemiBold");
+            fonts.AddFont("OpenSans-SemiboldItalic.ttf", "OpenSansSemiBoldItalic");
+
+            fonts.AddFont("OpenSans-Light.ttf", "OpenSansLight");
+            fonts.AddFont("OpenSans-LightItalic.ttf", "OpenSansLightItalic");
+
+            fonts.AddFont("OpenSans-Medium.ttf", "OpenSansMedium");
+            fonts.AddFont("OpenSans-MediumItalic.ttf", "OpenSansLight");
+
+            fonts.AddFont("OpenSans-Italic.ttf", "OpenSansItalic");
+
             fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            
         }).UseMauiCommunityToolkit();
 
         return builder.Build();
